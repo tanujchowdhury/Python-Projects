51a03afb34063cbd2110b76ce45a464b6c83b806
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-06-08 16:01:48 -0700

    Added database assignment

---
+++ database_assignment.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+#Establishing the connection
+conn = sqlite3.connect('test_database')
+
+with conn:
+    
+    #Creating a cursor object
+    cur = conn.cursor()
+
+    #Creating a table
+    cur.execute(" CREATE TABLE IF NOT EXISTS files ( \
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                Name VARCHAR(50) \
+                )")
+    conn.commit()
+conn.close()
+
+
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+#Puts all the .txt files in a list
+files_txt = []
+for i in fileList:
+    if i.endswith('.txt'):
+        files_txt.append(i)
+
+
+conn = sqlite3.connect('test_database')
+
+with conn:
+    
+   
+    cur = conn.cursor()
+    
+    #Prepare an SQL query to INSERT
+    cur.execute("INSERT INTO files(Name) VALUES (?)", \
+                (files_txt[0],))
+    cur.execute("INSERT INTO files(Name) VALUES (?)", \
+                (files_txt[1],))
+    conn.commit()
+conn.close()
+
+#Print file names to console
+print("File 1: {}\nFile 2: {}".format(files_txt[0], files_txt[1]))
